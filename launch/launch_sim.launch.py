1a73bab2040ab0de286e16915a5fa144ff71abd4
Pier Preissner <pierstudy@web.de>
2023-04-08 11:40:42 +0200

    new_launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -5,11 +5,11 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.actions import RegisterEventHandler
 from launch_ros.actions import Node
-
-
+from launch.event_handlers import (OnProcessExit, OnProcessStart)
 
 def generate_launch_description():
 
@@ -30,17 +30,25 @@ def generate_launch_description():
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
              )
 
+    load_joint_state_controller = ExecuteProcess(cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+                                                 'joint_broad'],
+                                                 output="screen")
+
+    load_arm_controller = ExecuteProcess(cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+                                                 'diff_cont'],
+                                                 output="screen")
+    
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
-
     # Launch them all!
     return LaunchDescription([
-        rsp,
+        RegisterEventHandler(event_handler=OnProcessExit(target_action=spawn_entity, on_exit=[load_joint_state_controller])),
+        RegisterEventHandler(event_handler=OnProcessExit(target_action=load_joint_state_controller, on_exit=[load_arm_controller])),
         gazebo,
-        spawn_entity,
+        rsp,
+        spawn_entity
     ])
